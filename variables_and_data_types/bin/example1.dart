e18d8cfe71ae1fb2b02b8e7e6efc85ef930ee550
Taimoork8 <kingtaimoor405@gmail.com>
2023-04-02 16:08:45 +0500

    Chapter about variables and data types

---
+++ variables_and_data_types/bin/example1.dart
@@ -0,0 +1,21 @@
+// constant example
+
+// void main(List<String> args) {
+//   const name = 'khan';
+//   print(name);
+// }
+
+// void main(List<String> args) {
+//   final age = 200;
+//   print(age);
+
+//   //this is also invalid
+//   // age = 20;
+// }
+
+void main(List<String> args) {
+  const String myName = 'Khan';
+  final mName = myName;
+  print(myName);
+  print(mName);
+}
